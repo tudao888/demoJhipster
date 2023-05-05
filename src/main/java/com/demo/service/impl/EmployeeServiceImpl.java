f656ca6d07292fbf0a16fd1ebf12599ff6cd6d12
tudao888 <daotu2106@gmail.com>
2023-05-05 18:08:04 +0700

    second

---
+++ src/main/java/com/demo/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,72 @@
+package com.demo.service.impl;
+
+import com.demo.domain.Employee;
+import com.demo.repository.EmployeeRepository;
+import com.demo.service.EmployeeService;
+import com.demo.service.dto.EmployeeDTO;
+import com.demo.service.mapper.EmployeeMapper;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final Logger log = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, EmployeeMapper employeeMapper) {
+        this.employeeRepository = employeeRepository;
+        this.employeeMapper = employeeMapper;
+    }
+
+    @Override
+    public EmployeeDTO save(EmployeeDTO employeeDTO) {
+        log.debug("Request to save Employee : {}", employeeDTO);
+        Employee employee = employeeMapper.toEntity(employeeDTO);
+        employee = employeeRepository.save(employee);
+        return employeeMapper.toDto(employee);
+    }
+
+    @Override
+    public EmployeeDTO update(EmployeeDTO employeeDTO) {
+        log.debug("Request to update Employee : {}", employeeDTO);
+        Employee employee = employeeMapper.toEntity(employeeDTO);
+        employee = employeeRepository.save(employee);
+        return employeeMapper.toDto(employee);
+    }
+
+    @Override
+    public Page<EmployeeDTO> findAll(Pageable pageable) {
+        log.debug("Request to get all Employees");
+        return employeeRepository.findAll(pageable).map(employeeMapper::toDto);
+    }
+
+    @Override
+    public Optional<EmployeeDTO> partialUpdate(EmployeeDTO employeeDTO) {
+        return Optional.empty();
+    }
+
+    //    @Override
+    //    public Optional<EmployeeDTO> partialUpdate(EmployeeDTO employeeDTO) {
+    //        log.debug("Request to partially update Employee : {}", employeeDTO);
+    //
+    //    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<EmployeeDTO> findOne(Long id) {
+        log.debug("Request to get Employee : {}", id);
+        return employeeRepository.findById(id).map(employeeMapper::toDto);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete Employee : {}", id);
+        employeeRepository.deleteById(id);
+    }
+}
